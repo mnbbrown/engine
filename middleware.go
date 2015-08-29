94a54a543f13ac750a8771d4b1cb15afe86bf14a
Matthew Brown <me@matthewbrown.io>
2015-08-29 14:23:56 +1000

    Minor changes to middleware

--- middleware.go
+++ middleware.go
@@ -2,7 +2,10 @@ package engine
 
 import (
 	"github.com/dchest/uniuri"
+	"log"
 	"net/http"
+	"strings"
+	"time"
 )
 
 func RequestIDMiddleware(next http.Handler) http.Handler {
@@ -11,6 +14,14 @@ func RequestIDMiddleware(next http.Handler) http.Handler {
 		GetContext(req).Set("Request-Id", id)
 		req.Header.Set("X-Request-Id", id)
 		next.ServeHTTP(rw, req)
+	})
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(rw, req)
+		log.Printf("%s %d", req.URL.Path, time.Now().Sub(start).Nanoseconds())
 	})
 }
 
@@ -27,4 +38,31 @@ func CORSAcceptAll(next http.Handler) http.Handler {
 			next.ServeHTTP(rw, req)
 		}
 	})
+}
+
+type CORSConfig struct {
+	AllowedMethods []string
+	AllowedOrigins []string
+}
+
+var AllowAllConfig = &CORSConfig{
+	AllowedMethods: []string{"*"},
+	AllowedOrigins: []string{"*"},
+}
+
+func CORSMiddleware(config *CORSConfig) MiddlewareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			switch {
+			case req.Method == "OPTIONS":
+				rw.Header().Set("Access-Control-Allow-Methods", strings.Join(config.AllowedMethods, ", "))
+				rw.Header().Set("Access-Control-Allow-Origins", strings.Join(config.AllowedOrigins, ", "))
+				return
+			default:
+				rw.Header().Set("Access-Control-Allow-Methods", strings.Join(config.AllowedMethods, ", "))
+				rw.Header().Set("Access-Control-Allow-Origins", strings.Join(config.AllowedOrigins, ", "))
+				next.ServeHTTP(rw, req)
+			}
+		})
+	}
 }
